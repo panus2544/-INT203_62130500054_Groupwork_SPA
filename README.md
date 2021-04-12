aaa9a7dc0388b1f5eaa16b1e76337178c30fd83f
panus2544 <panus.khjrs@mail.kmutt.ac.th>
2021-04-12 17:46:29 +0700

    Init project ✅✨🎊

--- README.md
+++ README.md
@@ -22,3 +22,4 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+"# INT203_62130500054_Groupwork_SPA" 
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,35 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 class="bg-sea">This is an {{ namePage }}</h1>
+    <ul v-for="post in posts" :key="post.id">
+      <li>{{ post.title }} :: author {{ post.author }}</li>
+    </ul>
   </div>
 </template>
+
+<script>
+const axios = require("axios").default;
+export default {
+  name: "About",
+  data() {
+    return {
+      namePage: "About Page",
+      posts: [],
+    };
+  },
+  methods: {
+    async getPosts() {
+      await axios.get("http://localhost:5000/posts").then((respone) => {
+        console.log(
+          "🚀 ~ file: About.vue ~ line 19 ~ getPosts ~ data",
+          respone
+        );
+        this.posts = respone.data;
+      });
+    },
+  },
+  created() {
+    this.getPosts();
+  },
+};
+</script>
